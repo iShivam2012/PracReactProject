2fdce86ecdc942dcdaedcc6baac18cf967666705
iShivam2012 <shivam.gupta97a@gmail.com>
2021-05-23 01:40:35 +0530

    Added AddUser component
    Its a form component where we can add username and age

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
-
+import React from "react";
+import AddUser from "./User/AddUser";
 
 function App() {
   return (
     <div>
-
+      <AddUser />
     </div>
   );
 }
---
+++ src/User/AddUser.js
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import Card from "../UI/Card";
+import Button from "../UI/Button";
+import styles from "./AddUser.module.css";
+
+const AddUser = () => {
+  const [userEntered, setUserEntered] = useState({
+    userName: "",
+    age: "",
+  });
+  const addUserHandler = (event) => {
+    if (
+      userEntered.userName.trim().length > 0 &&
+      userEntered.age &&
+      +userEntered.age > 1
+    ) {
+      console.log(userEntered);
+      setUserEntered({ userName: "", age: "" });
+      return event.preventDefault();
+    } else {
+      return event.preventDefault();
+    }
+  };
+  const userNameHandler = (event) => {
+    setUserEntered({ ...userEntered, userName: event.target.value });
+  };
+  const ageHandler = (event) => {
+    setUserEntered({ ...userEntered, age: event.target.value });
+  };
+  return (
+    <Card className={styles.input}>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="Username">Username</label>
+        <input
+          id="username"
+          value={userEntered.userName}
+          type="text"
+          onChange={userNameHandler}
+        />
+        <label htmlFor="Age">Age (Years)</label>
+        <input
+          id="age"
+          type="number"
+          value={userEntered.age}
+          onChange={ageHandler}
+        />
+        <Button type="submit">Add User</Button>
+      </form>
+    </Card>
+  );
+};
+
+export default AddUser;
